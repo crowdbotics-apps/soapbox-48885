76afcfcf91a0a917da12cafdd34dcdd3ee3859a1
Crowdbotics <team@crowdbotics.com>
2024-07-26 21:58:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soapbox-48885",
+  url: "https://soapbox-48885.botics.co",
+  api: "https://soapbox-48885.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
